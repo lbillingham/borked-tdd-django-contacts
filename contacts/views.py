cfe91b2a33df54c4baf6a0dfc95c93324e3a7469
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-29 12:06:11 +0000

    refactored to to home pae template
    `contacts` app now actually wired in in  `settings.py`

--- contacts/views.py
+++ contacts/views.py
@@ -1,9 +1,9 @@
 """
 bulding out home page, contacts etc.etc.
 """
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
     """no place like home"""
-    return HttpResponse('<html><title>Contacts</title><html>')
+    return render(request, 'home.html')
