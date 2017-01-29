357c3193da9b8579495a6cb3eb47a47eb3486b3d
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-29 11:52:40 +0000

    spelling corrections in user story

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,7 @@ with Selenium out of the box
 """
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 _TIME_WAIT_4_LOAD = 3  # seconds for browser to wait for pageload
 
@@ -23,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_add_new_organization_and_retrieve_later(self):
+    def test_can_add_new_organisation_and_retrieve_later(self):
         """test adding a contact and verifying persistence"""
         # Alex has heard about an new online address book
         # and checks out its homepage
@@ -31,22 +32,25 @@ class NewVisitorTest(unittest.TestCase):
 
         # Alex notices the page title mentions contacts
         self.assertIn('Contacts', self.browser.title)
+        # and the header mentions organisations
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Orginisations', header_text)
 
         # do more work on test
         self.fail('finish the test !!!11!')
-        # They are invited to enter a to-do item straight away
+        # They are invited to enter a Organisation Contact straight away
 
         # They type "Round Table." into a text box
 
         # When they hit enter, the page updates, and now the page lists
         # "Acme Inc." as an item
 
-        # There is another text box inviting her to add an email.
+        # There is another text box inviting them to add an email.
         # Alex enters "info@rtable.com"
 
-        # The page updates again, and now shows both items persisted
+        # The page updates again, and now shows both Contact and eamil persisted
 
-        # Alex wonders whether the site will remember her contact.
+        # Alex wonders whether the site will remember their contact.
         # Then they see that the site has generated a unique URL for them
         #  -- there is some explanatory text to that effect.
 
