5399ac20dd3699a6710d68911156cf0cc1dfb9d6
Laurence Billingham <laurence.billingham@gmail.com>
2017-01-30 22:45:15 +0000

    FTs for persistance and editing of Organisations

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -31,8 +31,8 @@ class NewVisitorTest(LiveServerTestCase):
         orgs = org_list.find_elements_by_tag_name('li')
         self.assertIn(item_text, [org.text for org in orgs])
 
-    def test_can_add_new_organisation_and_retrieve_later(self):
-        """test adding a contact and verifying persistence"""
+    def test_can_add_new_organisations_and_see_on_home_page(self):
+        """test adding 2 contacts shows up on home page"""
         # Alex has heard about an new online address book
         # and checks out its homepage
         self.browser.get(self.live_server_url)
@@ -82,14 +82,76 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Alex is returned to the home page agiain
         # it now shows both Organisations persisted
-        self.check_for_item_text_in_list('Round Table')
-        self.check_for_item_text_in_list('Cheese Shop')
+        self.check_for_item_text_in_list('Round Table [edit]')
+        self.check_for_item_text_in_list('Cheese Shop [edit]')
 
         # Alex wonders whether the site will remember their contacts.
         # Then they see that the site has generated a unique URL for them
         self.fail('finish the test!')
 
-        # They visit that URL - The "Round Table" contact is still there.
+    def test_organisations_added_can_be_edited(self):
+        """test can add an org then edit it"""
+        # Alex has heard about an new online address book
+        # and checks out its homepage
+        self.browser.get(self.live_server_url)
+        # Alex adds a new organisation by following a link
+        link = self.browser.find_element_by_id('id_add_organisation')
+        link.click()
+        namebox = self.browser.find_element_by_id('id_name')
+        emailbox = self.browser.find_element_by_id('id_email')
+        namebox.send_keys('Can I be edited')
+        namebox.send_keys(Keys.ENTER)
+        emailbox.send_keys('edit.me@example.com')
+        emailbox.send_keys(Keys.ENTER)
 
-        # Satisfied, Alex goes back to sleep
+        # When they hit enter, Alex is returned to the home page
+        # which now lists the new org
+        self.check_for_item_text_in_list('Can I be edited [edit]')
+        # Alex notices an edit link next to the new org
+        link = self.browser.find_element_by_id('id_edit_organisation')
+        link.click()
+        # they are redirected to a url
+        org_url = self.browser.current_url
+        self.assertRegex(org_url, '/edit-organisation/.+')
+        # Alex sees their organisation named
+        namebox = self.browser.find_element_by_id('id_name')
+        self.assertEqual(namebox.get_attribute('value'), 'Can I be edited')
+        # alex edits the organisations name
+        namebox.clear()
+        namebox.send_keys('Edited I have been')
+        namebox.send_keys(Keys.ENTER)
+        # Alex is sent back to the homepage and sees
+        # the edits have worked
+        self.check_for_item_text_in_list('Edited I have been [edit]')
 
+    def test_organisation_persistance(self):
+        """do orgs hang around after browser quits?"""
+        # Alex adds a new organisation
+        # clciks the link to its edit page
+        self.browser.get(self.live_server_url)
+        link = self.browser.find_element_by_id('id_add_organisation')
+        link.click()
+        namebox = self.browser.find_element_by_id('id_name')
+        emailbox = self.browser.find_element_by_id('id_email')
+        namebox.send_keys('Round Table')
+        namebox.send_keys(Keys.ENTER)
+        emailbox.send_keys('rtable@example.com')
+        emailbox.send_keys(Keys.ENTER)
+        edit_link = self.browser.find_element_by_id('id_edit_organisation')
+        edit_link.click()
+        # they are redirected to a url
+        org_url = self.browser.current_url
+        # Alex quits the browser
+        # later they return to the home page
+        # and find their added org still there
+        ## We use a new browser session to make sure that no information
+        ## of Alex's is coming through from cookies etc
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+        self.check_for_item_text_in_list('Round Table [edit]')
+        # alex returns to the orgs edit page and sees it
+        # is at the same url
+        edit_link = self.browser.find_element_by_id('id_edit_organisation')
+        edit_link.click()
+        self.assertEqual(org_url, self.browser.current_url)
